86fb38029761f6cf29f03d77bbcbaedf4ca19e55
Fmyh1828 <3256411401@qq.com>
2025-02-08 11:20:24 +0800

    update cmakefiles

---
+++ build/CMakeFiles/ChatBox.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "C:/Users/aa202/Desktop/Code/CodeC++/dazuoye/out/ChatBox.exe"
+  "C:/Users/aa202/Desktop/Code/CodeC++/dazuoye/out/ChatBox.exe.manifest"
+  "C:/Users/aa202/Desktop/Code/CodeC++/dazuoye/out/ChatBox.pdb"
+  "C:/Users/aa202/Desktop/Code/CodeC++/dazuoye/out/libChatBox.dll.a"
+  "CMakeFiles/ChatBox.dir/source/color.cpp.obj"
+  "CMakeFiles/ChatBox.dir/source/color.cpp.obj.d"
+  "CMakeFiles/ChatBox.dir/source/comment.cpp.obj"
+  "CMakeFiles/ChatBox.dir/source/comment.cpp.obj.d"
+  "CMakeFiles/ChatBox.dir/source/common.cpp.obj"
+  "CMakeFiles/ChatBox.dir/source/common.cpp.obj.d"
+  "CMakeFiles/ChatBox.dir/source/login.cpp.obj"
+  "CMakeFiles/ChatBox.dir/source/login.cpp.obj.d"
+  "CMakeFiles/ChatBox.dir/source/main.cpp.obj"
+  "CMakeFiles/ChatBox.dir/source/main.cpp.obj.d"
+  "CMakeFiles/ChatBox.dir/source/post.cpp.obj"
+  "CMakeFiles/ChatBox.dir/source/post.cpp.obj.d"
+  "CMakeFiles/ChatBox.dir/source/register.cpp.obj"
+  "CMakeFiles/ChatBox.dir/source/register.cpp.obj.d"
+  "CMakeFiles/ChatBox.dir/source/scene.cpp.obj"
+  "CMakeFiles/ChatBox.dir/source/scene.cpp.obj.d"
+  "CMakeFiles/ChatBox.dir/source/user.cpp.obj"
+  "CMakeFiles/ChatBox.dir/source/user.cpp.obj.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ChatBox.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
